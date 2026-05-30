a3084596ee8fa7475946083813568088e0c1ca63
Samanuri Sri Manasa Varma <manasasamanuri@gmail.com>
2026-05-30 07:53:21 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,12 +2,13 @@
 import { useState } from 'react';
 import { Hero } from '@/components/Hero';
 import { About } from '@/components/About';
+import { Experience } from '@/components/Experience';
 import { Skills } from '@/components/Skills';
 import { Projects } from '@/components/Projects';
 import { Contact } from '@/components/Contact';
 import { Navigation } from '@/components/Navigation';
 import { ThemeProvider } from '@/components/ThemeProvider';
-import { Experience } from '@/components/Experience';
+
 
 const Index = () => {
   return (
@@ -16,6 +17,7 @@ const Index = () => {
         <Navigation />
         <Hero />
         <About />
+        <Experience />
         <Skills />
         <Projects />
         <Contact />
